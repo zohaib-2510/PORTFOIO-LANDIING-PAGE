b12282dcd782b70c89394ac324ca9b0b380786bb
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-09-06 14:01:18 -0700

    2half

---
+++ app.js
@@ -0,0 +1,7 @@
+function omMk(){
+   var b = document.getElementById('plp').innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias dignissimos rem cupiditate pariatur quia quis, error, quisquam doloribus'
+
+
+  var a = document.getElementById('kml').style.display='none'
+
+}
